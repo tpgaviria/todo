fc3827b3b0b7e3a8fd2c6ef50870736e833fe4ee
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-06-04 13:28:58 -0400

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,25 @@ var mysql = require('mysql');
 require('dotenv').config();
 
 // mysql connection requirements - password hidden in .env file
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.sqlpass,
-    database: 'todo_list'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.sqlpass,
+        database: 'todo_list'
+    });
+}
 
 // log if connected or failed
-connection.connect(function(err){
-if (err) {
-    console.log('error connecting to my sql');
-    return;
-}
-console.log('conneted as id ' + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.log('error connecting to my sql');
+        return;
+    }
+    console.log('conneted as id ' + connection.threadId);
 });
 
 // exports connection for use in other files
